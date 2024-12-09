31dd695f00bccbfd3baf6228d5c1a9061bc1e283
beefchimi <curtdulmage@gmail.com>
2024-12-09 10:32:37 -0500

    :recycle: [Components] Update renamed file imports

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import {Footer} from '@src/components/sections/Footer/Footer.tsx';
 import {Header} from '@src/components/sections/Header/Header.tsx';
 import {Main} from '@src/components/sections/Main/Main.tsx';
 
-import {AccordionTest} from '@src/components/ui/Accordion/Accordion.test.tsx';
-import {ButtonTest} from '@src/components/ui/Button/Button.test.tsx';
+import {AccordionTest} from '@src/components/ui/Accordion/AccordionTest.tsx';
+import {ButtonTest} from '@src/components/ui/Button/ButtonTest.tsx';
 
 import styles from './App.module.css';
 
